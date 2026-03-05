c6094af7745f1cbb284a37839f4577c48c430876
ankur <ankurr.era@gmail.com>
2026-03-05 15:05:28 +0530

    Delete test_local.py

--- scraper/test_local.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('.')
-from scraper.makaut_scraper import fetch_from_url, parse_notices, BASE_URL
-
-html = fetch_from_url('https://www.makautexam.net/')
-notices = parse_notices(html, BASE_URL)
-for n in notices:
-    print("FOUND:", n)
